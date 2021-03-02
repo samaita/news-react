16af3a1b3398b9e908dc09295f7714cf1c52a56a
unknown <garysamaita@gmail.com>
2021-03-03 01:12:32 +0700

    Recent article now using mock data in src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
+import DataMockEnergy from './mock/sample.energy.json'
 
 function App() {
+
+	const RecentArticles = []
+
+	DataMockEnergy.articles.map(function (el) {
+		RecentArticles.push(
+			<div className="max-w-full bg-white border-2 border-gray-50 p-3 rounded-2xl tracking-wide shadow mb-3">
+				<div id="header" className="flex">
+					<div id="body" className="flex flex-col mr-8 w-7/12 h-full p-2">
+						<h2 id="category" className="font-light text-green-500 mb-1 leading-5">{el.source.name}</h2>
+						<h1 id="title" className=" font-semibold mb-1 leading-5">{el.title}</h1>
+						<h6 id="timestamp" className="text-xs font-light text-gray-400 object-left-bottom">{el.publishedAt}</h6>
+					</div>
+					<div class="bg-gray-100 w-5/12 h-32 max-h-96 block rounded-2xl bg-cover" style={{ backgroundImage: `url(${el.urlToImage})` }}></div>
+				</div>
+			</div >)
+	})
+
 	return (
 		<div className="App">
-			<div>
-				<div className="min-h-screen bg-gray-100 flex flex-col pr-3 pl-3 pt-10">
-					<h1 className="text-3xl font-light text-gray-800 mb-3">Recent</h1>
-					<div className="max-w-full bg-white border-2 border-gray-50 p-3 rounded-2xl tracking-wide shadow-md">
-						<div id="header" className="flex">
-							<div id="body" className="flex flex-col mr-8 h-full p-2">
-								<h2 id="category" className="text-md font-light text-gray-400 mb-1 leading-5">TechCrunch</h2>
-								<h1 id="title" className="text-xl font-semibold mb-1 leading-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-								<h6 id="category" className="text-xs font-light text-gray-400 object-left-bottom">February 23, 2021</h6>
-							</div>
-							<div className="w-screen h-full">
-								<img alt="mountain" className="object-contain rounded-xl" src="https://picsum.photos/seed/picsum/200" />
-							</div>
-						</div>
-					</div>
-				</div>
+			<div className="min-h-screen bg-gray-100 flex flex-col pr-3 pl-3 pt-10">
+				<h1 className="text-3xl font-light text-gray-800 mb-3">Recent</h1>
+				{RecentArticles}
 			</div>
 		</div>
 	);
