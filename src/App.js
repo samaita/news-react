bc7f3940191dc0001632b62e5f350eb312e1a396
unknown <garysamaita@gmail.com>
2021-03-04 23:47:43 +0700

    Hide default image if source not having the real image

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ function App() {
 	const RecentArticles = []
 
 	DataMockEnergy.articles.map(function (el) {
+		let hasDefaultImage = el.urlToImage.includes("default")
+
 		RecentArticles.push(
 			<div className="max-w-full bg-white border-2 border-gray-50 p-3 rounded-2xl tracking-wide shadow mb-3">
 				<div id="header" className="flex">
-					<div id="body" className="flex flex-col mr-8 w-7/12 h-full p-2">
-						<h2 id="category" className="font-light text-green-500 mb-1 leading-5">{el.source.name}</h2>
-						<h1 id="title" className=" font-semibold mb-1 leading-5">{el.title}</h1>
+					<div id="body" className={`flex flex-col mr-2 w-8/12 h-full p-2 ${hasDefaultImage ? "w-full" : "w-7/12"}`}>
+						<h2 id="category" className="font-light text-green-500 leading-5 text-xs">{el.source.name}</h2>
+						<h1 id="title" className="mb-1 text-base leading-5">{el.title}</h1>
 						<h6 id="timestamp" className="text-xs font-light text-gray-400 object-left-bottom">{el.publishedAt}</h6>
 					</div>
-					<div class="bg-gray-100 w-5/12 h-32 max-h-96 block rounded-2xl bg-cover" style={{ backgroundImage: `url(${el.urlToImage})` }}></div>
+					{hasDefaultImage ? "" : <div class="bg-gray-100 w-4/12 h-28 max-h-96 block rounded-2xl bg-cover" style={{ backgroundImage: `url(${el.urlToImage})` }}></div>}
 				</div>
 			</div >)
 	})
