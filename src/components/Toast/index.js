b6b4e2bf0e886a152545cad0c147a04658d8235a
unknown <garysamaita@gmail.com>
2021-03-27 00:17:57 +0700

    Props destructuring for Toast

--- src/components/Toast/index.js
+++ src/components/Toast/index.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types'
 
-const Toast = ({ toast, handleRemoveToast }) => {
+const Toast = (props) => {
     return (
         <div className="fixed left-0 p-2 w-full bottom-0 z-40" >
-            {true && toast.map(function (data) {
+            {props.toast.map(function (data) {
                 return (
-                    <div className="flex items-center mt-1 bg-red-500 border-l-4 border-red-700 py-2 px-3 shadow-md z-40">
+                    <div key={data.id} className="flex items-center mt-1 bg-red-500 border-l-4 border-red-700 py-2 px-3 shadow-md z-40">
                         <div className="flex item-center text-white text-sm content-center w-full">
-                            <div className="w-11/12">{data.message}</div><div onClick={() => handleRemoveToast(data.id)} className="w-1/12 text-center float-right block">OK</div>
+                            <div className="w-11/12">{data.message}</div><div onClick={() => props.handleRemoveToast(data.id)} className="w-1/12 text-center float-right block">OK</div>
                         </div>
                     </div>
                 )
@@ -23,7 +23,7 @@ Toast.propTypes = {
             message: PropTypes.string,
             type: PropTypes.string
         }
-    )),
+    ).isRequired),
     handleRemoveToast: PropTypes.func.isRequired
 }
 
